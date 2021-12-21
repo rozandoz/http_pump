ca517e8a31d8bcb17f6e03d3972329e1a3f32354
rozandoz <powerstatus524@gmail.com>
2021-12-21 01:20:13 +0100

    Add initial implementation of virtual file

---
+++ src/virtual_http_file/virtual_http_file.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+#include <tuple>
+#include <memory>
+#include <vector>
+
+#include "httplib.h"
+
+class VirtualHttpFile
+{
+    const char *ContetnLengthKey = "Content-Length";
+    const char *ContentTypKey = "Content-Type";
+
+    std::tuple<std::string, std::string> ParseUrl(const std::string &url);
+    httplib::Headers CreateRangeHeaders(size_t offset, size_t size);
+
+public:
+    explicit VirtualHttpFile();
+
+    void Open(const std::string &url);
+    std::vector<char> Read(size_t offset, size_t size);
+
+    size_t size() const { return size_; }
+    std::string type() const { return type_; }
+    httplib::Headers headers () const { return headers_; }
+
+private:
+    std::unique_ptr<httplib::Client> client_;
+    size_t size_;
+    std::string type_;
+    std::string path_;
+    httplib::Headers headers_;
+};
